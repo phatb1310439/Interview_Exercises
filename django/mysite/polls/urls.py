85f35f88253283b18494b13ade5694f751260971
phatb1310439 <thanhphatitsc@gmail.com>
2017-11-10 02:20:13 +0700

    tutorial 4

--- django/mysite/polls/urls.py
+++ django/mysite/polls/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
     #Removing hardcoded URLs in templates
     # url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
