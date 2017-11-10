ee060b190dc2d96d304c1a453e0acf46e8fc849c
phatb1310439 <thanhphatitsc@gmail.com>
2017-11-10 09:49:06 +0700

    automated testing

--- django/mysite/polls/urls.py
+++ django/mysite/polls/urls.py
@@ -3,11 +3,17 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'polls'
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'),
+#     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+#     #Removing hardcoded URLs in templates
+#     # url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+#     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+#     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+# ]
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
-    #Removing hardcoded URLs in templates
-    # url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
