f798c72d10cb02e86d53386e0effa85e5a9703d3
phatb1310439 <thanhphatitsc@gmail.com>
2017-11-10 10:32:06 +0700

    Customize the admin form

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -5,14 +5,15 @@ from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question, Choice
 from django.http import Http404
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
